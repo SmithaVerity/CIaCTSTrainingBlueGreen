8a2c9f8210928f16062bae9d7315c28a5c98c617
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-03-03 17:58:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -79,6 +79,37 @@ variable "enable_green_env" {
   default     = false
 }
 
+locals {
+  traffic_dist_map = {
+    blue = {
+      blue  = 100
+      green = 0
+    }
+    blue-90 = {
+      blue  = 90
+      green = 10
+    }
+    split = {
+      blue  = 50
+      green = 50
+    }
+    green-90 = {
+      blue  = 10
+      green = 90
+    }
+    green = {
+      blue  = 0
+      green = 100
+    }
+  }
+}
+
+variable "traffic_distribution" {
+  description = "Levels of traffic distribution"
+  type        = string
+}
+
+
 variable "green_instance_count" {
   description = "Number of instances in green environment"
   type        = number
